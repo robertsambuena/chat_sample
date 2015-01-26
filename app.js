e0242688b0b07647cc5a288977c5d6f4b410212d
robertsambuena <robertsambuena@gmail.com>
2015-01-26 17:02:17 +0800

    first

---
+++ app.js
@@ -0,0 +1,163 @@
+//dependencies
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect('mongodb://localhost/chat_db');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('callback');
+  var users_schema = mongoose.Schema({
+      id: String,
+      nickname: String,
+      created_at: Date,
+      sockets: []
+  });
+  var Users = mongoose.model('users', users_schema);
+  var new_user = new Users({
+    id: 12,
+    nickname: 'testboys',
+    created_at: new Date(),
+    sockets: ['h929svnert0fsdfo', 'urbvpn0378djqpbso3']
+  });
+  console.log('new_user',new_user);
+  new_user.save(function (err, res) {
+    if (err) return console.error(err);
+    console.log('new_user res',res);
+  });
+  Users.find(function (err, new_user) {
+    if (err) return console.error(err);
+    console.log('find_all', new_user);
+  });
+  Users.find({ name: /^testdata/ }, function (err, res) {
+    if (err) return console.error(err);
+    console.log('find', res);
+  });
+});
+
+
+//custom variables
+var __current_users = {};
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/js', function(req, res){
+  res.sendFile(__dirname + '/index.js');
+});
+app.get('/css', function(req, res){
+  res.sendFile(__dirname + '/index.css');
+});
+
+//convert object to array
+function obj_to_array (obj) {
+  if ( obj!== null && typeof obj === 'object')
+    return Object.keys(obj).map(function (key) {return obj[key]});
+  return false;
+}
+
+
+// SOCKET DECLARATION
+var User = io.of('/default_user');
+User.on('connection', function(socket) {
+  //join default_room
+  socket.join('default_room');
+
+  /* Catch events from client */
+  //connect
+  socket.on('connect', function(user){
+    console.log('user connected');
+  });
+
+  //disconnect
+  socket.on('disconnect', function() {
+
+    console.log('user disconnected', __current_users);
+  });
+
+
+  //someone connected
+  socket.on('user_connect', function(user) {
+    //send to everyone except the sender
+    if (!user.sockets) user.sockets = []; //initialize if not declared user.sockets
+    if (user && user.id) {
+      var specific_user = chatuser.get(user.id);
+      user.sockets.push(socket.id); //save new socket to user
+      if (specific_user) user = specific_user;
+      else chatuser.edit(user.id, user);
+    }
+    socket.in('default_room').emit('user_connect', user); //send event
+  });
+
+  //someone sent a message
+  socket.on('chat_message', function(chat) {
+    //send to everyone except the sender
+    socket.in('default_room').emit('chat_message', chat);
+  });
+
+  socket.on('get_current_users', function(chat) {
+    //send to everyone except the sender
+    check_all_user_status();
+  });
+});
+
+//user resource
+//__current_users will be replaced with a real database resource
+function ChatUser () {
+  this.get = function (id) {
+    if (id) return __current_users;
+    else return __current_users[id] || false
+  };
+  this.add = function (user) {
+    return __current_users[user.id] = user;
+  };
+  this.edit = function (id, user) {
+    if (__current_users[id]) return __current_users[id].user = user;
+    return false;
+  };
+  this.get_by_socket = function (sid) {
+    //get current online users
+    var __current_users_arr = obj_to_array(__current_users);
+    return __current_users_arr.filter(function (arr) {
+         return !!~arr.sockets.indexOf(sid);
+    });
+  };
+  this.remove_socket = function (sid) {
+    //get current online users
+    var __current_users_arr = obj_to_array(__current_users);
+    return __current_users_arr.filter(function (arr) {
+        var result = arr.sockets.indexOf(sid)
+        if (!!~result){
+          __current_users[__current_users_arr.id].sockets[result].splice(result, 1);
+        }
+        return true;
+    });
+  };
+}
+
+var chatuser = new ChatUser();
+
+//get current online users
+function check_all_user_status() {
+  var socket_ids = Object.keys(User.adapter.rooms['default_room']),
+      online_users = [],
+      __current_users_arr = obj_to_array(chatuser.get());
+      console.log('__current_users',__current_users);
+  //check the socket_ids from users
+  socket_ids.forEach(function(sid) {
+    online_users.push(chatuser.get_by_socket(sid));
+    console.log('online_users', online_users.length);
+  });
+  console.log('send event \'current online\': ' + __current_users_arr.length);
+  //send to all sockets
+  io.of('/default_user').in('default_room').emit('get_current_users', online_users);
+}
+
+setInterval(check_all_user_status, 5000);
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ index.js
@@ -0,0 +1,175 @@
+var socket = io('http://192.168.1.124:3000/default_user');
+	e_message_form = document.getElementById('message_form'),
+	e_message_box = document.getElementById('messages'),
+	e_userlist = document.getElementById('user-list'),
+	user = {}, check_user = null;
+
+// Chat class declaration
+function Chat () {
+	this.get_messages = function (raw) {
+		var chat = JSON.parse(localStorage.getItem('chat'));
+		chat = chat?chat:[];
+		if (raw) {
+			chat.sort(function(a,b){
+			  return new Date(b.crtd_at) - new Date(a.crtd_at);
+			});
+		}
+		return chat;
+	};
+	this.add_message = function (chat_obj) {
+		var chat = JSON.parse(localStorage.getItem('chat'));
+		chat = chat?chat:[];
+		console.log('chat_obj',chat_obj);
+		chat.push(chat_obj);
+		localStorage.setItem('chat', JSON.stringify(chat));
+		return true;
+	};
+	this.get_user = function () {
+		return JSON.parse(localStorage.getItem('user'));
+	};
+	this.set_user = function (user) {
+		console.log('set_user',user);
+		localStorage.setItem('user', JSON.stringify(user));
+		return true;
+	};
+	this.reset = function (argument) {
+		localStorage.setItem('user', null);
+		localStorage.setItem('chat', null);
+		return true;
+	};
+}
+
+//instantiate Chat class
+var chat_storage = new Chat();
+
+//send message
+e_message_form.addEventListener("submit", function(event) {
+	var chat_sending = {
+		'msg': e_message_form.m.value,
+		'nick': user.nickname
+	};
+	socket.emit('chat_message', chat_sending);
+	e_message_form.m.value = '';
+	//save message locally
+	append_a_message(chat_sending);
+	chat_storage.add_message({
+    	'type' : 'message',
+    	'data' : chat_sending,
+    	'crtd_at' : new Date()
+    });
+	event.preventDefault();
+}, false);
+
+//receive message
+socket.on('chat_message', function(chat) {
+	//save message locally
+    append_a_message(chat);
+    chat_storage.add_message({
+    	'type' : 'message',
+    	'data' : chat,
+    	'crtd_at' : new Date()
+    });
+});
+
+//someone connect
+socket.on('user_connect', function(user) {
+	console.log('use1',user);
+	append_an_announcement(user);
+    chat_storage.add_message({
+    	'type' : 'announcement',
+    	'data' : user,
+    	'crtd_at' : new Date()
+    });
+});
+
+//get current users
+socket.on('get_current_users', function(users) {
+	console.log('use1rs',users);
+	e_userlist.innerHTML = '<div class="users-box-content"><a href="">' +users.nickname+ '</a></div>';
+});
+
+//connected!
+socket.on('connected1', function(sid) {
+	console.log('sid',sid);
+	// init(sid);
+});
+
+//on close 
+function closing (event) {
+	console.log('event',event);
+	alert('WTF');
+    socket.emit('user_disconnect', {
+		'nick': nickname
+	});
+}
+//on close events
+window.addEventListener("onbeforeunload", closing);
+window.addEventListener("onunload", closing);
+
+function append_an_announcement (user) {
+	e_message_box.innerHTML = e_message_box.innerHTML + '<li class="announcement"><span class="msg"><b>'+user.nick+'</b> is now connected.</span></li>';
+	document.getElementById('popup-messages').scrollTop = document.getElementById('popup-messages').scrollHeight + 99999;
+}
+
+function append_a_message (chat) {
+	var row_msg_class = "";
+	if (chat.nick===user.nickname) {
+		row_msg_class = 'self';
+		e_message_box.innerHTML = e_message_box.innerHTML + '<li class="'+row_msg_class+'"><span class="msg">' + chat.msg + '</span></li>';
+	}
+    else {
+    	e_message_box.innerHTML = e_message_box.innerHTML + '<li class="'+row_msg_class+'"><span class="nick">' + chat.nick + ': </span><span class="msg">' + chat.msg + '</span></li>';
+    }
+  	document.getElementById('popup-messages').scrollTop = document.getElementById('popup-messages').scrollHeight + 99999;
+}
+
+function refresh_page (e_message_box, chat_obj) {
+	e_message_box.innerHTML = '';
+	if (chat_obj) {
+		chat_obj.forEach(function(chat) {
+		    if (chat.type=='announcement') {
+				append_an_announcement(chat.data);
+			}
+			else if (chat.type=='message') {
+				append_a_message(chat.data);
+			}
+		});
+	}
+}
+
+function reset_chat () {
+	chat_storage.reset();
+	e_message_box.innerHTML = '';
+}
+
+function get_current_users () {
+	console.log('get_current_users');
+	socket.emit('get_current_users', null);
+}
+
+//start
+function init (sid) {
+	check_user = chat_storage.get_user();
+	if (check_user) {
+		console.log('App use existing data');
+		user = check_user;
+		check_user = null;
+		refresh_page(e_message_box, chat_storage.get_messages());
+	}
+	else {
+		console.log('App Restarted');
+		user.nickname = prompt("Nickname: ");
+		if (user.nickname) {
+			user.created_at = new Date();
+			// user.id = sid;
+			var random = Math.random().toString(36).slice(2);
+			socket.emit('user_connect', {
+				'id': random + 'bert',
+				'nick': user.nickname
+			});
+			chat_storage.set_user(user);
+		}
+	}
+}
+
+init();
